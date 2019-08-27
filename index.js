3a498c896e9d1d15109d5b302c9a512bcf42fe1b
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-08-27 13:27:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    update()
+})
+
+function update(){
+    document.getElementById("text").innerHTML = "This is really cool!"
+}
+
